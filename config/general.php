f603fba37531c4920074bd86618ca6e60e8fec8d
pgm-glenmega <glenmega@student.arteveldehs.be>
2025-02-24 19:10:45 +0100

    added filters for event/sermons and past/upcoming + searchbar

--- config/general.php
+++ config/general.php
@@ -1,27 +1,21 @@
 <?php
 /**
- * General Configuration
- *
- * All of your system's general configuration settings go in here. You can see a
- * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
- *
- * @see \craft\config\GeneralConfig
+ * General Configuration for CraftCMS
  */
 
 use craft\config\GeneralConfig;
 use craft\helpers\App;
 
 return GeneralConfig::create()
-    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
-    ->defaultWeekStartDay(1)
-    // Prevent generated URLs from including "index.php"
-    ->omitScriptNameInUrls()
-    // Preload Single entries as Twig variables
-    ->preloadSingles()
-    // Prevent user enumeration attacks
-    ->preventUserEnumeration()
-    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->defaultWeekStartDay(1) // Set the default start of the week (0 = Sunday)
+    ->omitScriptNameInUrls() // Remove "index.php" from URLs
+    ->preloadSingles() // Preload single entries
+    ->preventUserEnumeration() // Security feature
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
+        'enableCsrfProtection' => false,    
     ])
+    ->enableCsrfProtection(true) // Ensure CSRF is enabled
+    ->devMode(true) // Enable debug mode (disable on production!)
+    ->useEmailAsUsername(true) // Ensure emails can be used as usernames
 ;
